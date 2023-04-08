0b0d2c8d8ae8c1c6105074451c6bb6139c7b61bb
developergaurav77 <developer.gaurav77@gmail.com>
2023-04-08 14:06:52 +0545

    user_part_added

--- Home_Page.py
+++ Home_Page.py
@@ -10,13 +10,12 @@ def set_streamlit_page_config_once():
             return
         raise e
 set_streamlit_page_config_once()
-
 import pandas as pd
 from sqlalchemy import create_engine, text
 import plotly.graph_objects as go
 
 def connection():
-    password = "**"
+    password = "****"
     engine = create_engine(f'mysql+pymysql://root:{password}@localhost:3306/anpr')
     return engine
 
@@ -31,32 +30,50 @@ st.markdown("# Dashboard")
 st.sidebar.markdown("# Dashboard")
 
 
-query = 'SELECT * from anpr.car_check_in order by id desc;'
+
+query = 'SELECT * from anpr.car_check_in order by user_id desc;'
+query_user_reg = 'SELECT * from anpr.user_registation order by user_id desc;'
 with engine.begin() as conn:
     df = pd.read_sql_query(sql=text(query), con=conn)  
-df['id'] = df['id'].astype(int) 
-df = df.reset_index(drop=True)
+    df_user = pd.read_sql_query(sql=text(query_user_reg), con=conn)  
 
-daily_agg_df = df.groupby([pd.Grouper(key='check_in',freq="1D")]).agg(count=("id","count"),amount=("amount","sum")).reset_index()
+df['user_id'] = df['user_id'].astype(int) 
+df_user['user_id'] = df_user['user_id'].astype(int) 
+
+daily_agg_df = df.groupby([pd.Grouper(key='check_in',freq="1D")]).agg(count=("user_id","count"),amount=("amount","sum")).reset_index()
 daily_agg_df.sort_values("check_in",inplace=True,ascending=False)
 daily_agg_df.reset_index(drop=True,inplace=True)
 
-col1, col2 = st.columns( [0.5, 0.5])
-with col1:
-    total_count = df['id'].count()
+vehicle_entry_field, total_amount_field,total_user_registered = st.columns( [0.4, 0.4,0.2])
+with vehicle_entry_field:
+    total_count = df['user_id'].count()
     st.subheader("Total Vehicle Entry")
     st.write(total_count)
 
 
-with col2:
+with total_amount_field:
     total_amount = df['amount'].sum()    
     st.subheader("Total Amount Earned")
     st.write(total_amount)
 
-df_home = df.head(20)
-print(df_home)
-st.write("Latest Vehicle Entry:")
-st.dataframe(df_home)
+with total_user_registered:
+    user_reg_count = df_user['user_id'].count()
+    st.subheader("Total User Registration")
+    st.write(user_reg_count)
+
+
+total_check_in, total_user_reg = st.columns([0.5,0.5])
+with total_check_in:
+    df_home = df.head(20)
+    st.write("Latest Vehicle Entry:")
+    df_home.set_index("user_id",inplace=True)
+    st.dataframe(df_home)
+with total_user_reg:
+    st.write("Latest User Registration:")
+    df_user = df_user.head(20)
+    df_user.set_index("user_id",inplace=True)
+    st.dataframe(df_user)
+
 
 st.subheader("Vechile Entry Distribution")
 count_plot = plot(daily_agg_df['check_in'],daily_agg_df['count'])
--- pages/Check_In_Vehicle.py
+++ pages/Check_In_Vehicle.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from Home_Page import set_streamlit_page_config_once
 set_streamlit_page_config_once()
-
+# from User_Registration import user_reg
 import pandas as pd
 import time
 st.markdown("# Add New Car")
@@ -20,11 +20,16 @@ engine = connection()
 if "visibility" not in st.session_state:
     st.session_state.visibility = "visible"
     st.session_state.disabled = False
+    st.session_statelabel_visibility='collapse'
 
-checkin_time = datetime.now()
-date_format_i = '%Y-%m-%d, %H:%M'
-check_in_i = checkin_time.strftime("%Y-%m-%d, %H:%M")
-check_in_use = datetime.strptime(check_in_i, "%Y-%m-%d, %H:%M") #datetime datatype
+def check_in_time():
+    checkin_time = datetime.now()
+    date_format_i = '%Y-%m-%d, %H:%M'
+    check_in_i = checkin_time.strftime("%Y-%m-%d, %H:%M")
+    check_in_use = datetime.strptime(check_in_i, "%Y-%m-%d, %H:%M") #datetime datatype
+    return check_in_use
+
+charge_list = [20,40,50,100,150,180,200,220,250]
 def fare_charge(time_s):
     if time_s <= 0.5:
         charge = charge_list[0]
@@ -46,12 +51,38 @@ def fare_charge(time_s):
         charge = charge_list[8]    
     return charge
 
+def check_out_time(check_in_use):
+    diff_time = [0.5,1,2,4,6,8,10,12]
+    amount = []
+    check_out_time_list = []
+    check_out_time_only = []
+    for time_s in diff_time:
+        checkout_time = check_in_use + timedelta(hours=time_s)
+        time_s = (((checkout_time-check_in_use).seconds)/60)/60
+        amount = fare_charge(time_s)
+        co_t = str(checkout_time)
+        show_in_checkout = (time_s,amount,co_t)
+        check_out_time_list.append(show_in_checkout)
+        check_out_time_only.append(co_t)
+
+    checkout_s = st.selectbox(
+    'When would you like to Checkout?',check_out_time_only)
+    position_checkout = check_out_time_only.index(checkout_s)
+    for_time = check_out_time_list[position_checkout][0]
+    amount = check_out_time_list[position_checkout][1]
+
+    return checkout_s,for_time,amount
+
+
+
 def upload_image_file():
     uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
     return uploaded_file
 
+
 uploaded_file = upload_image_file()
 number_plate,accuracy = calulate_number_plate(uploaded_file)
+check_in_use = check_in_time()
 
 if uploaded_file is not None:
     if len(number_plate) == 0:
@@ -64,64 +95,97 @@ if uploaded_file is not None:
         st.write(f"Algorithm accuracy : {accuracy}%")
     number_plates = st.text_input("Algorithm output(number plate)",number_plate)
 
-    col1, col2, col3 = st.columns( [0.3, 0.2,0.4])
-    
-    with col1:
-        default_name = "Unknown"
-        car_owner_name = st.text_input(
-            "Car Owner Name",
-            f"{default_name}",
-            key="placeholder",
-        )
-        if car_owner_name:
-            st.write("Your name is: ", car_owner_name)
-    with col2:
-        st.write("Checkin time_s")
-        st.write(check_in_use)
+    # user = user_reg()
+    is_registered = st.selectbox("Is registered Already?",("No","Yes"))
+    if is_registered == 'No':
+        col1, address_field,col2, col3 = st.columns( [0.3,0.2, 0.2,0.3])
+        
+        with col1:
+            default_name = "Unknown"
+            car_owner_name = st.text_input(
+                "Car Owner Name",
+                f"{default_name}",
+                key="placeholder",
+            )
+            if car_owner_name:
+                st.write("Your name is: ", car_owner_name)
+        with address_field:
+            default_name = "Unknown"
+            address = st.text_input(
+                "Address",
+                f"{default_name}",
+                key="address",
+            )
+        with col2:
+            st.write("Checkin time")
+            st.write(check_in_use)
 
-    with col3:
-        diff_time = [0.5,1,2,4,6,8,10,12]
-        charge_list = [20,40,50,100,150,180,200,220,250]
-        amount = []
-        check_out_time_list = []
-        check_out_time_only = []
-        for time_s in diff_time:
-            checkout_time = check_in_use + timedelta(hours=time_s)
-            time_s = (((checkout_time-check_in_use).seconds)/60)/60
-            amount = fare_charge(time_s)
-            co_t = str(checkout_time)
-            show_in_checkout = (time_s,amount,co_t)
-            check_out_time_list.append(show_in_checkout)
-            check_out_time_only.append(co_t)
+        with col3:
+            diff_time = [0.5,1,2,4,6,8,10,12]
+            charge_list = [20,40,50,100,150,180,200,220,250]
+            amount = []
+            check_out_time_list = []
+            check_out_time_only = []
+            for time_s in diff_time:
+                checkout_time = check_in_use + timedelta(hours=time_s)
+                time_s = (((checkout_time-check_in_use).seconds)/60)/60
+                amount = fare_charge(time_s)
+                co_t = str(checkout_time)
+                show_in_checkout = (time_s,amount,co_t)
+                check_out_time_list.append(show_in_checkout)
+                check_out_time_only.append(co_t)
 
-        checkout_s = st.selectbox(
-        'When would you like to Checkout?',check_out_time_only)
-        position_checkout = check_out_time_only.index(checkout_s)
-        for_time = check_out_time_list[position_checkout][0]
-        amount = check_out_time_list[position_checkout][1]
+            checkout_s = st.selectbox(
+            'When would you like to Checkout?',check_out_time_only)
+            position_checkout = check_out_time_only.index(checkout_s)
+            for_time = check_out_time_list[position_checkout][0]
+            amount = check_out_time_list[position_checkout][1]
 
-        st.write(f'Checkout time_s selected: {checkout_s} and Amount : Rs {amount}')
-        st.write(f'Total time_s to be parked: {for_time} hr')
+            st.write(f'Checkout time selected: {checkout_s} and Amount : Rs {amount}')
+            st.write(f'Total time to be parked: {for_time} hr')
 
-    result = []
+        result = []
 
-    if len(number_plate) == 0:
-        st.write("Please Enter number plate and process further")
-    if len(car_owner_name) == 0:
-        car_owner_name = default_name
+        if len(number_plate) == 0:
+            st.write("Please Enter number plate and process further")
+        if len(car_owner_name) == 0:
+            car_owner_name = default_name
 
-    car_owner_name = str(car_owner_name)
-    number_plate_s = str(number_plates)
-    result.append([number_plate_s,check_in_use,checkout_s,amount,car_owner_name])
+        car_owner_name = str(car_owner_name)
+        number_plate_s = str(number_plates)
+        result.append([number_plate_s,check_in_use,checkout_s,amount,car_owner_name,address])
 
-    query_r = "SELECT * from anpr.car_check_in order by id desc limit 1"
-    if st.button('Create Entry'):
-        df = pd.DataFrame(result,columns=["vehicle_number","check_in","check_out","amount","vehicle_owner_name"])
-        df.to_sql("car_check_in",schema='anpr',index=False,if_exists='append', con=engine)
-        time.sleep(2)
-        st.write("Entry Sucessfully created")
-        time.sleep(2)
+        query_r = "SELECT * from anpr.car_check_in order by user_id desc limit 1"
+        if st.button('Create Entry'):
+            df = pd.DataFrame(result,columns=["vehicle_number","check_in","check_out","amount","vehicle_owner_name","address"])
+            df.to_sql("car_check_in",schema='anpr',index=False,if_exists='append', con=engine)
+            time.sleep(2)
+            st.write("Entry Sucessfully created")
+            time.sleep(2)
 
-        entered_df = pd.DataFrame(engine.connect().execute(text(query_r)))
-        st.dataframe(entered_df)
+            entered_df = pd.DataFrame(engine.connect().execute(text(query_r)))
+            st.dataframe(entered_df)
+
+    else:
+        provided_user_id = st.text_input("Please enter user id")
+        print(provided_user_id)
+        if provided_user_id:
+            query = f'SELECT * from anpr.user_registation where user_id = {int(provided_user_id)};'
+            with engine.begin() as conn:
+                df = pd.read_sql_query(sql=text(query), con=conn)  
+            if len(df.dropna()) == 0:
+                st.write(f"User id : {provided_user_id} doesn't found.")
+            else:
+                st.write("User registration found, changing parking status to 1")
+                df['vehicle_number'] = str(number_plates)
+                st.dataframe(df)
+                df.drop(columns=['user_id','created_on'],inplace=True)
+
+                df.to_sql("car_check_in",schema='anpr',index=False,if_exists='append', con=engine)
+
+                new_user_reg = pd.DataFrame(engine.connect().execute(text(f"SELECT * FROM anpr.car_check_in where user_reg_id = '{df['user_reg_id'].iloc[0]}'")))
+                
+                st.dataframe(new_user_reg)
+                st.write("Successfully checked in")
+
 
